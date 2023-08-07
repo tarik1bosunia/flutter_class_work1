17d25dfdc9e9b8b537b8c1790d726c531884e143
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-08-07 17:36:44 +0600

    make app pretier

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,21 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: "Namer App",
         theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)
-        ),
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green)),
         home: MyHome(),
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier{
+class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHome extends StatelessWidget {
@@ -35,14 +39,48 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
     return Scaffold(
-      body: Column(
-        children: [
-          Text("A random Idea:"),
-          Text(appState.current.asLowerCase)
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
